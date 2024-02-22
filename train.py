5bcf0c6f303b52f4654664a41677a0f4e393b07e
archana0311 <archana.jaipur@gmail.com>
2024-02-22 20:46:13 +0530

    First commit

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
 
 
 def train():
@@ -13,9 +16,9 @@ def train():
     data = pd.read_csv("data/properties.csv")
 
     # Define features to use
-    num_features = ["nbr_frontages"]
-    fl_features = ["fl_terrace"]
-    cat_features = ["equipped_kitchen"]
+    num_features = ["primary_energy_consumption_sqm","garden_sqm", "cadastral_income"]
+    fl_features = ["fl_terrace", "fl_floodzone"]
+    cat_features = ["province"]
 
     # Split the data into features and target
     X = data[num_features + fl_features + cat_features]
@@ -26,17 +29,31 @@ def train():
         X, y, test_size=0.20, random_state=505
     )
 
+ 
     # Impute missing values using SimpleImputer
+
+    numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
     imputer = SimpleImputer(strategy="mean")
     imputer.fit(X_train[num_features])
     X_train[num_features] = imputer.transform(X_train[num_features])
     X_test[num_features] = imputer.transform(X_test[num_features])
+
+
 
     # Convert categorical columns with one-hot encoding using OneHotEncoder
     enc = OneHotEncoder()
     enc.fit(X_train[cat_features])
     X_train_cat = enc.transform(X_train[cat_features]).toarray()
     X_test_cat = enc.transform(X_test[cat_features]).toarray()
+
+  # Standardisation
+    scaler = StandardScaler()
+    scaler.fit(X_train[num_features])
+    X_train_standardized = scaler.transform(X_train[num_features])
+    X_test_standardized = scaler.transform(X_test[num_features])
 
     # Combine the numerical and one-hot encoded categorical columns
     X_train = pd.concat(
@@ -55,6 +72,8 @@ def train():
         axis=1,
     )
 
+    
+    
     print(f"Features: \n {X_train.columns.tolist()}")
 
     # Train the model
